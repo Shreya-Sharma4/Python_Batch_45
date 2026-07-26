bff696751fce13835aec8be606228130c6db28a9
Rutuja Jagdale <your-email@example.com>
2026-07-26 13:59:32 +0530

    Add program to find second largest number

---
+++ second_largest.py
@@ -0,0 +1,9 @@
+numbers = list(map(int, input("Enter numbers separated by space: ").split()))
+
+unique_numbers = list(set(numbers))
+
+if len(unique_numbers) < 2:
+    print("There is no second largest number.")
+
+    unique_numbers.sort()
+    print("Second largest number is:", unique_numbers[-2])
