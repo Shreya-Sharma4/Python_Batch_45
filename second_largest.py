6036ec838a025b28aabc97575f52cf372d05a3ad
Rutuja Jagdale <your-email@example.com>
2026-07-26 16:25:45 +0530

    Fix review comments

--- second_largest.py
+++ second_largest.py
@@ -4,6 +4,6 @@ unique_numbers = list(set(numbers))
 
 if len(unique_numbers) < 2:
     print("There is no second largest number.")
-
+else:
     unique_numbers.sort()
     print("Second largest number is:", unique_numbers[-2])
